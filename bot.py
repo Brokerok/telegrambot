43c6d11559c974dd321d0e43ec416b977cc0941f
Pavlo Shev4uk <95368807+Brokerok@users.noreply.github.com>
2022-06-14 21:00:58 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,28 @@
+import telebot
+import config
+from telebot import types
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    sti = open('sticker.webp', 'rb')
+    bot.send_sticker(message.chat.id, sti)
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton('Пампушка')
+    item2 = types.KeyboardButton("Как дела?")
+
+    markup.add(item1, item2)
+
+    bot.send_message(message.chat.id, 'Welcome, {0.first_name}!'.format(message.from_user, bot.get_me()),
+                     parse_mode='html', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def lalala(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+bot.polling(none_stop=True)
